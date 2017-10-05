66ef781fb2f29011cf68a174234e9c3c34fb2812
ingevers4 <ingevers4@student.arteveldehs.be>
2017-10-05 10:09:21 +0200

    WIP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php $hallo = "hallo ";
+$wereld = "wereld";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $hallo , $wereld ?> <br>
+</body>
+</html>
